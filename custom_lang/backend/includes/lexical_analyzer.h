416a0a2ada23e038675a5da51d3c1db72d47cd0f
Genorto <89106200809gleb@gmail.com>
2024-10-25 09:05:59 +0300

    Add : class LexicalAnalyzer

---
+++ custom_lang/backend/includes/lexical_analyzer.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include "lexem.h"
+
+class LexicalAnalyzer {
+ public:
+     /*
+     Тут у нас борчик, разбиение на лексемки и т.п.
+     */
+
+ private:
+     std::vector<Lexem*> list_;
+};
--- custom_lang/main.cpp
+++ custom_lang/main.cpp
@@ -1,6 +1,7 @@
 #include "backend/includes/lexem.h"
 
 int main() {
+    std::string garbage = "while";
     Lexem* lexem = new Lexem;
     return 0;
 }
