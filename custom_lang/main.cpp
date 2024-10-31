0cef3fe3b8ba5f619225574a2970e4202b4070f2
HungryMan7 <osin.ba@ftl.name>
2024-10-31 13:55:09 +0300

    Update: lexical analyzer

--- custom_lang/main.cpp
+++ custom_lang/main.cpp
@@ -1,37 +1,12 @@
-#include "backend/includes/lexical_analyzer.h"
+#include <iostream>
+#include <vector>
+
+#include "Bor.h"
+#include "Lexem.h"
+#include "LexicalAnalyzer.h"
 
 int main() {
-    LexicalAnalyzer* la = new LexicalAnalyzer("custom_lang/resources/program.txt");
-    la->SetLanguage("custom_lang/resources/lang.txt");
+    LexicalAnalyzer* la = new LexicalAnalyzer("Program.txt");
+    la->SetLanguage("Utilities.txt");
     la->Analyze();
-    std::vector<Lexem*> checked_lexems = la->GetLexems();
-    for (auto x : checked_lexems) {
-        std::cout << x->GetValue() << " ";
-        switch (x->GetType()) {
-        case LexemType::Identifier:
-            std::cout << "identifier" << "\n";
-            break;
-
-        case LexemType::Utility:
-            std::cout << "utility" << "\n";
-            break;
-
-        case LexemType::Operator:
-            std::cout << "operator" << "\n";
-            break;
-
-        case LexemType::Literal:
-            std::cout << "literal" << "\n";
-            break;
-
-        case LexemType::Comment:
-            std::cout << "comment" << "\n";
-            break;
-
-        case LexemType::Empty:
-            std::cout << "empty" << "\n";
-            break;
-        }
-    }
-    return 0;
 }
