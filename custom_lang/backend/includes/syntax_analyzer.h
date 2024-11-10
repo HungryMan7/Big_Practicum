e54b0e5ea0355de487a1fd1b8ae3c2ec388e3e32
HungryMan7 <osin.ba@ftl.name>
2024-11-10 12:04:52 +0300

    Add: syntax base

---
+++ custom_lang/backend/includes/syntax_analyzer.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "LexicalAnalyzer.h"
+#include "Lexem.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+class SyntaxAnalyzer {
+    public:
+        SyntaxAnalyzer(std::string file_name);
+        virtual ~SyntaxAnalyzer();
+        void ReadFile(std::string file_name);
+        void Analyze();
+
+        /* setters */
+
+        /* getters */
+
+    private:
+        void GetLex();
+        void START();
+        void EXP_ZERO();
+        void EXP_ONE();
+        void EXP_TWO();
+        void EXP_THREE();
+        void EXP_FOUR();
+        void EXP_FIVE();
+        void EXP_SIX();
+        void EXP_SEVEN();
+        void EXP_EIGHT();
+        void EXP_NINE();
+        void EXP_TEN();
+        void EXP_ELEVEN();
+        void EXP_TWELVE();
+        void EXP_THIRTEEN();
+        void EXP_FOURTEEN();
+        char* program_, *iter_;
+        Lexem* lex_iter_;
+        Lexem lexem_;
+        std::vector<Lexem*> list_of_lexems_;
+        int file_size_, current_size_ = 0;
+};
+
