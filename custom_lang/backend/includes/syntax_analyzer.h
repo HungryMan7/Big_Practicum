bacc8b727cfdcd4873911cc7f596af383187c7ac
HungryMan7 <osin.ba@ftl.name>
2024-11-15 07:49:05 +0300

    Fixed analyzer: added loops

--- custom_lang/backend/includes/syntax_analyzer.h
+++ custom_lang/backend/includes/syntax_analyzer.h
@@ -21,6 +21,9 @@ private:
     void FUNC_CALL();
     void IF();
     void LOOP();
+    void LOOP_WHILE();
+    void LOOP_FOR();
+    void LOOP_FOREACH();
     void SWITCH();
     void ID();
     void TERM();
