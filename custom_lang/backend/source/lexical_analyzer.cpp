4fe8e0c68f89f19f6ce9dc8627df0666122a6b99
Genorto <89106200809gleb@gmail.com>
2024-12-08 18:40:04 +0300

    Fix : endless cycle in GetChar()

--- custom_lang/backend/source/lexical_analyzer.cpp
+++ custom_lang/backend/source/lexical_analyzer.cpp
@@ -54,7 +54,7 @@ void LexicalAnalyzer::GetChar() {
                     " wrong lexem";
             }
         }
-        return;
+        throw "OK!\n";
     }
     symbol_ = *iter_;
     ++iter_;
