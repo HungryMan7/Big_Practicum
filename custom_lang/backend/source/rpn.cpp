6881057aa9cc85512a7a22e80cd85ddd6cbcb945
Genorto <89106200809gleb@gmail.com>
2024-12-24 23:39:06 +0300

    Fix : unary '-' RPN

--- custom_lang/backend/source/rpn.cpp
+++ custom_lang/backend/source/rpn.cpp
@@ -919,20 +919,24 @@ void RPN::EXP_TWELVE(std::vector<RPN_Node*> &container) {
         GetLex();
     }
     std::vector<std::string> new_oper;
+    std::vector<std::string> signs;
     for (int i = 0; i < oper.size(); ++i) {
         if (oper[i] == "0") {
             RPN_Node* elem = new RPN_Node(oper[i], "integer");
             AddCell(elem, container);
         } else {
             if (oper[i] == "+" || oper[i] == "-") {
-                RPN_Node* operation = new RPN_Node(oper[i], "other");
-                AddCell(operation, container);
+                signs.push_back(oper[i]);
             } else {
                 new_oper.push_back(oper[i]);
             }
         }
     }
     EXP_THIRTEEN(container);
+    for (int i = 0; i < signs.size(); ++i) {
+        RPN_Node* operation = new RPN_Node(signs[i], "other");
+        AddCell(operation, container);
+    }
     for (int i = 0; i < new_oper.size(); ++i) {
         RPN_Node* operation = new RPN_Node(new_oper[i], "other");
         AddCell(operation, container);
