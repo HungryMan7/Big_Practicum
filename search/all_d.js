14df0a2e900c716c9e478f4ea0b3403ca31f9211
HungryMan7 <HungryMan7@users.noreply.github.com>
2024-12-25 07:34:46 +0000

    deploy: 14f688d60b31ddc6d7dfee906ddfbef99da0af11

--- search/all_d.js
+++ search/all_d.js
@@ -13,7 +13,7 @@ var searchData=
   ['setname_10',['setName',['../classCell.html#a1a7777aadf2846b617657f047654a958',1,'Cell']]],
   ['setstringvalue_11',['SetStringValue',['../structRPN__Node.html#a300c7e3039840c9103fa0dbe35d07edb',1,'RPN_Node']]],
   ['settid_12',['setTID',['../classRPN.html#ace0e08ba178672f8696d80427dc949d5',1,'RPN']]],
-  ['settype_13',['settype',['../classLexem.html#a9418e8d7ca4d1aed8fec336bc3b265bd',1,'Lexem::SetType()'],['../classCell.html#abaa5d8f26a1002f44c0396514b5de6f8',1,'Cell::setType()'],['../classTID.html#a4b187394a8e786ee6b2372d782debfaf',1,'TID::setType()']]],
+  ['settype_13',['settype',['../classCell.html#abaa5d8f26a1002f44c0396514b5de6f8',1,'Cell::setType()'],['../classTID.html#a4b187394a8e786ee6b2372d782debfaf',1,'TID::setType()'],['../classLexem.html#a9418e8d7ca4d1aed8fec336bc3b265bd',1,'Lexem::SetType(LexemType)']]],
   ['setvalue_14',['setvalue',['../classLexem.html#a2edc792496af70aaa75a3314e9a7ece4',1,'Lexem::SetValue()'],['../classRPN__TID.html#a3b424fe5f90b7eac07450d3ab9154111',1,'RPN_TID::setValue()'],['../classCell.html#a9cad0ab8b3562d36c1aae5fc6565e630',1,'Cell::setValue()'],['../classTID.html#a862f10f12ba9ad97222cd96a0591d464',1,'TID::setValue()']]],
   ['setvariant_15',['setVariant',['../classCell.html#abc01353e7fdb048ac5f3a20f0c6c08c8',1,'Cell']]],
   ['solve_16',['Solve',['../classInterpreter.html#a9a00ef619bafb645f64868648d4cf8b3',1,'Interpreter']]],
