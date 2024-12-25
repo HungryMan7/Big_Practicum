a8007c21d84063d5e0afd24237478ed87ac17547
Genorto <Genorto@users.noreply.github.com>
2024-12-25 20:10:01 +0000

    deploy: 1404abed27b9fdbde16f971c324dbc7e589de3b8

--- search/functions_9.js
+++ search/functions_9.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['setboolvalue_0',['SetBoolValue',['../structRPN__Node.html#a4dd8827af4bf92fcd7dbaa5522f4546f',1,'RPN_Node']]],
-  ['setcolumn_1',['setcolumn',['../classCell.html#a824329c7a92c8ea466c996460856fa2c',1,'Cell::setColumn()'],['../classLexem.html#a040150125042b6118984b68e0403e9cb',1,'Lexem::SetColumn()']]],
+  ['setcolumn_1',['setcolumn',['../classLexem.html#a040150125042b6118984b68e0403e9cb',1,'Lexem::SetColumn()'],['../classCell.html#a824329c7a92c8ea466c996460856fa2c',1,'Cell::setColumn()']]],
   ['setdoublevalue_2',['SetDoubleValue',['../structRPN__Node.html#af0624af7639573e89fb61e4a5be2a6ef',1,'RPN_Node']]],
   ['setintegervalue_3',['SetIntegerValue',['../structRPN__Node.html#a9b156959a6bd22cb60155e0c1c05a350',1,'RPN_Node']]],
   ['setlanguage_4',['SetLanguage',['../classLexicalAnalyzer.html#ad817ae8993fe944388414a634156500f',1,'LexicalAnalyzer']]],
@@ -9,8 +9,8 @@ var searchData=
   ['setname_6',['setName',['../classCell.html#a1a7777aadf2846b617657f047654a958',1,'Cell']]],
   ['setstringvalue_7',['SetStringValue',['../structRPN__Node.html#a300c7e3039840c9103fa0dbe35d07edb',1,'RPN_Node']]],
   ['settid_8',['setTID',['../classRPN.html#ace0e08ba178672f8696d80427dc949d5',1,'RPN']]],
-  ['settype_9',['settype',['../classCell.html#abaa5d8f26a1002f44c0396514b5de6f8',1,'Cell::setType()'],['../classTID.html#a4b187394a8e786ee6b2372d782debfaf',1,'TID::setType()'],['../classLexem.html#a9418e8d7ca4d1aed8fec336bc3b265bd',1,'Lexem::SetType(LexemType)']]],
-  ['setvalue_10',['setvalue',['../classLexem.html#a2edc792496af70aaa75a3314e9a7ece4',1,'Lexem::SetValue()'],['../classRPN__TID.html#a3b424fe5f90b7eac07450d3ab9154111',1,'RPN_TID::setValue()'],['../classCell.html#a9cad0ab8b3562d36c1aae5fc6565e630',1,'Cell::setValue()'],['../classTID.html#a862f10f12ba9ad97222cd96a0591d464',1,'TID::setValue()']]],
+  ['settype_9',['settype',['../classCell.html#abaa5d8f26a1002f44c0396514b5de6f8',1,'Cell::setType()'],['../classTID.html#a4b187394a8e786ee6b2372d782debfaf',1,'TID::setType()'],['../classLexem.html#a9418e8d7ca4d1aed8fec336bc3b265bd',1,'Lexem::SetType()']]],
+  ['setvalue_10',['setvalue',['../classRPN__TID.html#a3b424fe5f90b7eac07450d3ab9154111',1,'RPN_TID::setValue()'],['../classCell.html#a9cad0ab8b3562d36c1aae5fc6565e630',1,'Cell::setValue()'],['../classTID.html#a862f10f12ba9ad97222cd96a0591d464',1,'TID::setValue()'],['../classLexem.html#a2edc792496af70aaa75a3314e9a7ece4',1,'Lexem::SetValue()']]],
   ['setvariant_11',['setVariant',['../classCell.html#abc01353e7fdb048ac5f3a20f0c6c08c8',1,'Cell']]],
   ['solve_12',['Solve',['../classInterpreter.html#a9a00ef619bafb645f64868648d4cf8b3',1,'Interpreter']]]
 ];
