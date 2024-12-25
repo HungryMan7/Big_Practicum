52e2cdfccc6a1bf7c287558d13a9f21acccbf351
Genorto <89106200809gleb@gmail.com>
2024-12-26 01:55:23 +0300

    Fix : RunFunc() type

--- custom_lang/backend/includes/interpreter.h
+++ custom_lang/backend/includes/interpreter.h
@@ -4,7 +4,7 @@
 class Interpreter {
 public:
     void Run(std::vector<RPN_Node*> rpn);
-    void RunFunc(std::string, int);
+    RPN_Node* RunFunc(std::string, int);
     RPN_Node* Solve(RPN_Node*, RPN_Node*, std::string);
 
 private:
