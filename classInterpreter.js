18b953e0f7411c2ab65df7ee8de12dcac22e1af2
Genorto <Genorto@users.noreply.github.com>
2024-12-26 11:39:04 +0000

    deploy: 38db0d523083aed9bb409ee589631ac5e645fd2e

--- classInterpreter.js
+++ classInterpreter.js
@@ -1,6 +1,6 @@
 var classInterpreter =
 [
-    [ "Run", "classInterpreter.html#ace8b4dc5acd5db78739dab8eb9c5eb3c", null ],
-    [ "RunFunc", "classInterpreter.html#ab114d807e1a36ac7cea760b89a222436", null ],
+    [ "Run", "classInterpreter.html#a4c39cf3f0f886cbff6f4218855eddd20", null ],
+    [ "RunFunction", "classInterpreter.html#ae3053667ca5fbf1c5eaae56f73055798", null ],
     [ "Solve", "classInterpreter.html#a81223dfc119d8d5db97a6240632820a0", null ]
 ];
